e6050736b7aef4b7d6dee2fb6d1aaae12a737ab1
Khan <khan7038@gmail.com>
2022-05-05 11:01:41 +0530

    first commit

---
+++ src/Practice/Practice_git.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Practice_git {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter your number");
+		
+		System.out.println(sc.hasNext());          
+			
+	}
+
+}
